7a879f9f8949cd7ffc147d98c1dd159138626800
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-04-03 14:18:15 +0200

    Link show issue in github 💡

--- front/src/components/item/Issue.vue
+++ front/src/components/item/Issue.vue
@@ -3,6 +3,12 @@
     <h3 v-if="issue.repository">{{issue.repository.name}}</h3>
     <h4>{{issue.title}}</h4>
     <em v-html="marked(issue.body)"></em>
+    <a class="leftlink" :href="issue.html_url" target="_blank">
+      <i class="fa fa-link" aria-hidden="true"></i>
+      <span>view</span>
+      <span>on</span>
+      <i class="fa fa-github" aria-hidden="true"></i>
+    </a>
   </blockquote>
 </template>
 
@@ -40,6 +46,9 @@ blockquote{
   background: white;
   padding: 10px 20px;
   box-shadow: 0 0 1px rgba(0,0,0,0.25);
+  position: relative;
+  overflow: hidden;
+
   h3{
     margin: 0;
     padding: 0;
@@ -54,6 +63,36 @@ blockquote{
     max-width: 100%;
   }
 
+  &:hover{
+    .leftlink{
+      right: 0;
+    }
+  }
+
+  .leftlink{
+    transition: all 0.2s;
+    position: absolute;
+    right: -50px;
+    height: 100%;
+    background: #42b983;
+    color: white;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    .fa-link{
+      margin-bottom: 10px;
+    }
+    span{
+      transform: rotate(90deg);
+      display: block;
+      padding: 5px 0px;
+      padding-bottom: 9px;
+      font-weight: 700;
+    }
+  }
 }
 
 </style>
