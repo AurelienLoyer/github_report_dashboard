fbfb180c07d0ebdd90e60bd8fd6fa9ffe9b59cd9
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-04-04 13:56:13 +0200

    Use new component and add infinite scroll pagination

--- front/src/components/Repos.vue
+++ front/src/components/Repos.vue
@@ -1,67 +1,30 @@
 <template lang="html">
   <div>
-    <h2>Repos</h2>
-
-    <ul class="repos" v-if="!progress">
-      <li v-for="repo in repos">
-        <v-repo :repo="repo"></v-repo>
-      </li>
-    </ul>
-    <div v-if="repos.length === 0 && !progress" class="">
-      -----
-    </div>
-
-    <v-loader v-if="progress"></v-loader>
-    <v-quota v-if="meta" :meta="meta"></v-quota>
-
+    <v-reposlist :url="'repos'" :title="'Repos'"></v-reposlist>
   </div>
 </template>
 
 <script>
 
-import env from 'env'
-import auth from '../auth'
-import Quota from './Quota.vue'
-import Loader from './Loader.vue'
-import Repo from './item/Repo.vue'
+import ReposList from './item/ReposList.vue'
 
 export default {
   name: 'repos',
   data () {
     return {
-      repos: [],
-      meta: null,
-      token: localStorage.getItem('token'),
-      progress: true
     }
   },
   components: {
-    'v-quota':Quota,
-    'v-loader':Loader,
-    'v-repo':Repo
+    'v-reposlist' : ReposList
   },
   created(){
-    this.$http.get(env.api+'/repos'+auth.urlToken()).then(response => {
-      this.repos = response.body.data
-      console.log(this.repos)
-      this.meta = response.body.meta
-      this.progress = false
-    })
+  },
+  destroyed () {
   },
   methods:{
-
   }
-
 }
 </script>
 
 <style lang="scss">
-
-  .repos{
-    text-align: left;
-    width: 1000px;
-    margin: auto;
-    max-width: 100%;
-  }
-
 </style>
--- front/src/components/filters/date.js
+++ front/src/components/filters/date.js
@@ -4,6 +4,5 @@ import moment from 'moment'
 Vue.filter('date', function (value,format) {
   if (!value) return ''
   console.log(format)
-  //let format = 'DD/MM/YYYY'
   return moment(value).format('YYYY-MM-DD HH:mm')
 }
